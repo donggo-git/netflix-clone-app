955b67183026d2b83f1ac5d7d5efc5a4e9da2ded
Dong <Dlttthvlhhth@gmail.com>
2021-09-13 20:42:57 -0700

    add Promise when fetching in SectionPage

--- src/SectionPage.js
+++ src/SectionPage.js
@@ -1,17 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BASE_URL, key } from './request'
 
 function SectionPage({ section }) {
+    const [movies, setMovies] = useState([])
     const fetchSection = async (section) => {
         switch (section) {
             case 'TV Shows':
                 fetch(`${BASE_URL}discover/tv?api_key=${key}&sort_by=popularity.desc`)
+                    .then(doc => {
+                        doc.json().then(data => setMovies(data.results))
+                    })
                 break;
             case "Movies":
                 fetch(`${BASE_URL}discover/movie?api_key=${key}&sort_by=popularity.desc`)
+                    .then(doc => {
+                        doc.json().then(data => setMovies(data.results))
+                    })
                 break;
             case "Latest":
                 fetch(`${BASE_URL}discover/movie?api_key=${key}&primary_release_year=2021`)
+                    .then(doc => {
+                        doc.json().then(data => setMovies(data.results))
+                    })
         }
 
     }
