1e871dba05997161c03b3b1ecac788a70957aa0a
Dong <Dlttthvlhhth@gmail.com>
2021-09-24 19:45:05 -0700

    html element for searchList and style them

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -24,6 +24,9 @@ function SearchMovies({ isSearchOpen }) {
                     })
                 })
         }
+        else {
+            setSearchList([])
+        }
     }
     const handleChange = (input) => {
         setSearchInput(input)
@@ -33,6 +36,16 @@ function SearchMovies({ isSearchOpen }) {
     return (
         <div className="searchMovies" style={isSearchOpen ? searchOpen : searchClose}>
             <input type='text' value={searchInput} onChange={(e) => handleChange(e.target.value)} />
+            <div className="searchMovies_movieList"
+                style={searchList.length > 0 ? { height: 'calc(100vh - 57.5px)' }
+                    : { height: '0' }}>
+                {searchList.length > 0 ? searchList.map(movie => (
+                    <div className="searchMovies_movie">
+                        <img src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} height='100%' />
+                        <p>{movie?.name || movie?.original_title || movie?.original_name || movie?.title}</p>
+                    </div>
+                )) : <div></div>}
+            </div>
         </div>
     )
 }
